1530c7cb1785d932de513eaf79d274e28322dafb
Geeta Qltech <geeta@qltech.com.au>
2021-08-10 18:48:05 +0530

    First G B Commit 10-08

---
+++ backup/09-08-2021_HAR_kinsta/thankyou_old.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ * Thankyou page
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/checkout/thankyou.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.7.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+?>
+
+<div class="woocommerce-order">
+
+	<?php
+	if ( $order ) :
+
+		do_action( 'woocommerce_before_thankyou', $order->get_id() );
+		?>
+
+		<?php if ( $order->has_status( 'failed' ) ) : ?>
+
+			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php esc_html_e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
+
+			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
+				<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php esc_html_e( 'Pay', 'woocommerce' ); ?></a>
+				<?php if ( is_user_logged_in() ) : ?>
+					<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php esc_html_e( 'My account', 'woocommerce' ); ?></a>
+				<?php endif; ?>
+			</p>
+
+		<?php else : ?>
+
+			
+
+			<!-- <p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p> -->
+
+			<section class="inner_page thankyou_page">
+  <div class="container">
+    <div class="thankyoupagecnt">
+      <span class="thankyouicon"><img src="<?php echo get_template_directory_uri() . '/assets/images/Thank you.jpg'?>"></span>
+      <h1>Thank you</h1>
+      
+			<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+      <p>HAPPY? LET YOUR FRIENDS KNOW!</p>
+      <ul class="thankyousociallist">
+        <!-- <li><a href="https://www.facebook.com/HAMPL-Manufacturer-of-Natural-Animal-Medicines-173523929397518/" target="_blank"><i class="fab fa-facebook-f"></i></a></li> -->
+        <!-- <li><a href="#."><i class="fab fa-twitter"></i></a></li>
+        <li><a href="#."><i class="fab fa-linkedin"></i></a></li> -->
+       <!--  <li><a href="mailto:support@hampl.com.au"><i class="fab fa-google"></i></a></li> -->
+        <?php echo do_shortcode('[Sassy_Social_Share]'); ?>
+      </ul>
+      <div class="thankorderbtn"><a class="btn-main whitebtn" id="order_detailshide">order Details</a></div>
+    </div>
+  </div>
+</section>
+			<div class="order_show" id="order_show" style="display: none;">
+				<div class="bankddetails">
+					<?php $order_id =  $order->get_order_number();
+
+					$order = wc_get_order( $order_id );
+
+					$data  = $order->get_data();
+
+					$billing_country    = $data['billing']['country'];
+
+					//echo $billing_country;
+					$country_array = array();
+					$check  = false;
+
+					$args = array(  
+        'post_type' => 'bankdetails',
+        'post_status' => 'publish',
+        'posts_per_page' => 8, 
+        'orderby' => 'title', 
+        'order' => 'ASC', 
+    );
+					 $loop = new WP_Query( $args ); 
+    /*echo "<pre>";
+    print_r($loop);*/
+        
+    while ( $loop->have_posts() ) : $loop->the_post(); 
+        /*echo "if";
+        print the_title(); 
+        the_excerpt(); */
+   
+                ?>
+                
+                	<h4><?php $country_code = get_field('country_code')?></h4>
+                	<?php
+                	$country_array = array();
+                	$country_array[] = $country_code;
+					if($billing_country==$country_code)
+					{
+						$check = true;
+						?>
+							 <h4><?php echo get_field('bank_details')?></h4>
+						<?php
+					}
+					
+                 endwhile;
+
+    wp_reset_postdata(); 
+
+   					if($check==false)
+   					{
+
+		    				if(!in_array($billing_country,$country_array))
+		    				{
+		    					?>
+		    					<h4><?php echo get_field('bank_details','7181')?></h4>
+		    					<?php
+		    				}
+		    		}
+    ?>
+
+				</div>
+			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
+
+				<li class="woocommerce-order-overview__order order">
+					<?php esc_html_e( 'Order number:', 'woocommerce' ); ?>
+					<strong><?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+				</li>
+
+				<li class="woocommerce-order-overview__date date">
+					<?php esc_html_e( 'Date:', 'woocommerce' ); ?>
+					<strong><?php echo wc_format_datetime( $order->get_date_created() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+				</li>
+
+				<?php if ( is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email() ) : ?>
+					<li class="woocommerce-order-overview__email email">
+						<?php esc_html_e( 'Email:', 'woocommerce' ); ?>
+						<strong><?php echo $order->get_billing_email(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+					</li>
+				<?php endif; ?>
+
+				<li class="woocommerce-order-overview__total total">
+					<?php esc_html_e( 'Total:', 'woocommerce' ); ?>
+					<strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+				</li>
+
+				<?php if ( $order->get_payment_method_title() ) : ?>
+					<li class="woocommerce-order-overview__payment-method method">
+						<?php esc_html_e( 'Payment method:', 'woocommerce' ); ?>
+						<strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
+					</li>
+				<?php endif; ?>
+
+			</ul>
+
+		<?php endif; ?>
+
+		<?php do_action( 'woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id() ); ?>
+		<?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
+
+	<?php else : ?>
+
+		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), null ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+
+
+	<?php endif; ?>
+
+</div>
+
+</div>
+
+<script type="text/javascript">
+	
+	 $("#order_detailshide").click(function(){
+
+	 	if ($('#order_show').hasClass('order_show'))
+	 	{
+	 		$('#order_show').show();
+	 		$('#order_show').removeClass('order_show');
+	 	}
+	 	else
+	 	{
+	 		$('#order_show').hide();
+	 		$('#order_show').addClass('order_show');
+	 	}
+
+	 });
+</script>
